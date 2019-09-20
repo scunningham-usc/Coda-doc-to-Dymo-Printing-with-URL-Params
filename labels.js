083e95d790a157af561a59bcbe12dc89fe2bedf4
Ben Lee <7084778+benblee@users.noreply.github.com>
2019-09-20 13:56:03 -0700

    Added function to GET query string parameters
    Used code from the David Walsh Blog to grab URL parameters.
    https://davidwalsh.name/query-string-javascript

--- labels.js
+++ labels.js
@@ -102,3 +102,10 @@ function dymoPrint() {
 	});
 	return false;
 }
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
